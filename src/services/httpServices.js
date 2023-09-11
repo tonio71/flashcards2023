a083872481e54f31c2521cf1a0cf29d90e8b9aaa
tonio71 <tonio.maria@gmail.com>
2023-09-11 12:46:22 -0300

    Update httpServices.js

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
         //baseURL:'http://localhost:3001',
-        baseURL:`${process.env.BACKEND_URL}`,
+        baseURL:"https://flashcard2023-vercel-backend.vercel.app",
         timeout:10000
 })
 
