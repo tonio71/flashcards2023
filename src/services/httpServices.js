a53b976f5524a35ac4116e51ba93df5d9b33d091
tonio71 <tonio.maria@gmail.com>
2023-09-11 12:36:44 -0300

    Update httpServices.js

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-        baseURL:'http://localhost:3001',
+        //baseURL:'http://localhost:3001',
+        baseURL:process.env.BACKEND_URL,
         timeout:10000
 })
 
